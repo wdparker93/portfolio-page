b33bd19d6d7ae63e6bd58ee5ea70257d524f24cc
wdparker <wdparker@ncsu.edu>
2022-08-06 23:39:36 -0500

    Added professional links

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./index.css";
 import python from "./images/python-icon.png";
 import java from "./images/java-icon.png";
 import c from "./images/c-icon.png";
-import htmljscss from "./images/html-js-css-icon.png";
-import react from "./images/react-icon.png";
-import geodivline from "./images/geometric-divider-line.png";
+import felang from "./images/fe-languages.png";
+import linkedin from "./images/linked-in-icon.png";
+import github from "./images/github-icon.png";
+import gmail from "./images/gmail-icon.png";
 
 function App() {
   return (
@@ -13,60 +14,89 @@ function App() {
       <div class="title-section">
         <div id="main-heading">Will Parker's Portfolio Page</div>
         <div id="main-heading-overlay">Will Parker's Portfolio Page</div>
-        <div id="secondary-heading">Proojet Overview</div>
-        <div id="secondary-heading-overlay">Project Overview</div>
+        <div id="secondary-heading">Developer Overview</div>
+        <div id="secondary-heading-overlay">Developer Overview</div>
       </div>
-      <div class="project-overview">
-        <ul id="icon-list">
-          <li>
-            <img src={python} id="python-icon" alt="Python Icon" />
-            <a
-              class="link"
-              href="https://github.com/stars/wdparker93/lists/python-projects"
-            >
-              Python Projects
-            </a>
-          </li>
-          <li>
-            <img src={java} id="java-icon" alt="Java Icon" />
-            <a
-              class="link"
-              href="https://github.com/stars/wdparker93/lists/java-projects"
-            >
-              Java Projects
-            </a>
-          </li>
-          <li>
-            <img src={c} id="c-icon" alt="C Icon" />
-            <a
-              class="link"
-              href="https://github.com/stars/wdparker93/lists/c-projects"
-            >
-              C Projects
-            </a>
-          </li>
-          <li>
-            <img src={htmljscss} id="fe-icon" alt="HTML JS CSS Icon" />
-            <a class="link" href="https://codepen.io/wdparker93">
-              Front-End Web Dev Projects
-            </a>
-          </li>
-          <li>
-            <img src={react} id="react-icon" alt="React Icon" />
-            <a class="link" href="https://codepen.io/wdparker93">
-              Front-End Web Dev Projects
-            </a>
-          </li>
-        </ul>
-        <div>
-          <img
-            src={geodivline}
-            id="geo-div-line"
-            alt="Geometric Divider Line"
-          />
+      <div id="content-wrapper">
+        <div class="project-overview">
+          <div class="window-title" id="project-window-title">
+            Project Summary
+          </div>
+          <ul id="icon-list">
+            <li>
+              <img src={python} id="python-icon" alt="Python Icon" />
+              <a
+                class="link"
+                href="https://github.com/stars/wdparker93/lists/python-projects"
+              >
+                Python Projects
+              </a>
+            </li>
+            <li>
+              <img src={java} id="java-icon" alt="Java Icon" />
+              <a
+                class="link"
+                href="https://github.com/stars/wdparker93/lists/java-projects"
+              >
+                Java Projects
+              </a>
+            </li>
+            <li>
+              <img src={c} id="c-icon" alt="C Icon" />
+              <a
+                class="link"
+                href="https://github.com/stars/wdparker93/lists/c-projects"
+              >
+                C Projects
+              </a>
+            </li>
+            <li>
+              <img src={felang} id="fe-icon" alt="HTML JS CSS Icon" />
+              <a class="link" id="fe-link" href="https://codepen.io/wdparker93">
+                Front-End Web Dev Projects
+              </a>
+            </li>
+          </ul>
         </div>
-        <textarea value="Some more text" />
+        <div class="professional-links">
+          <div class="window-title" id="professional-window-title">
+            Networking Links
+          </div>
+          <ul id="link-list">
+            <li>
+              <img src={linkedin} id="linkedin-icon" alt="LinkedIn Icon" />
+              <a
+                class="link"
+                id="linkedin-link"
+                href="https://www.linkedin.com/in/william-parker-215877aa"
+              >
+                LinkedIn Profile
+              </a>
+            </li>
+            <li>
+              <img src={github} id="github-icon" alt="Github Icon" />
+              <a
+                class="link"
+                id="github-link"
+                href="https://github.com/wdparker93"
+              >
+                Github Page
+              </a>
+            </li>
+            <li>
+              <img src={gmail} id="gmail-icon" alt="Gmail Icon" />
+              <a
+                class="link"
+                id="gmail-link"
+                href="mailto: wdparker93@gmail.com"
+              >
+                Email wdparker93@gmail.com
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
+      <hr id="horizontal-break-1"></hr>
     </div>
   );
 }
